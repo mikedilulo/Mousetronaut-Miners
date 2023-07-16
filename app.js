43c82977eedb83cf0e88387f5699f604a700ad21
michaeladilulo <michaeladilulo@gmail.com>
2023-07-16 01:56:19 -0600

    Mousetronaut Auto Upgrade ready for Set Interval

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let cheeseCount = 0;
+let mousetronautCount = 0;
 
 const gameUpgrades = {
     knives: {
@@ -17,6 +18,8 @@ let knifePurchasePrice = gameUpgrades.knives.purchasePrice;
 let knifeActivated = gameUpgrades.knives.activated;
 let knifeClickModifier = gameUpgrades.knives.clickModifier;
 let mousetronautPurchasePrice = gameUpgrades.mousetronauts.purchasePrice;
+let mousetronautActivated = gameUpgrades.mousetronauts.activated;
+let mousetronautClickModifier = gameUpgrades.mousetronauts.clickModifer;
 
 const totalCheeseCount = document.querySelector('.cheese-count');
 const clickToGenerateCheeseResource = document.getElementById('cheeseResource');
@@ -26,6 +29,8 @@ const mousetronautUpgradeButton = document.querySelector('.cheese-mousetronaut-u
 
 disableButtonsAtStart();
 
+
+
 clickToGenerateCheeseResource.addEventListener('click', () => {
 mineCheese();
 });
@@ -34,11 +39,22 @@ knifeUpgradeButton.addEventListener('click', () => {
     purchaseCheeseKnifeUpgrade();
 })
 
+mousetronautUpgradeButton.addEventListener('click', () => {
+    purchaseCheeseMousetronautUpgrade();
+})
+
 function mineCheese() {
     cheeseCount++;
     totalCheeseCount.innerHTML = cheeseCount;
     enableButtonsForUpgrades();
     isKnifeActivated();
+    isMousetronautActivated();
+}
+
+function mousetronautDeactivated() {
+    if(mousetronautCount > 0) {
+        mousetronautUpgradeButton.disabled = true;
+    }
 }
 
 function disableButtonsAtStart() {
@@ -49,19 +65,41 @@ function disableButtonsAtStart() {
 
 function purchaseCheeseKnifeUpgrade() {
     knifeActivated = true;
-    cheeseCount -= knifePurchasePrice;
-    mineCheese();
-    return knifeActivated;
+    cheeseCount = cheeseCount -= knifePurchasePrice;
+    totalCheeseCount.innerHTML = cheeseCount;
+    return cheeseCount;
+}
+
+function purchaseCheeseMousetronautUpgrade() {
+    mousetronautCount++;
+    mousetronautDeactivated();
+    mousetronautActivated = true;
+    cheeseCount = cheeseCount -= mousetronautPurchasePrice;
+    totalCheeseCount.innerHTML = cheeseCount;
+    return cheeseCount;
 }
 
 function enableButtonsForUpgrades() {
     if(cheeseCount >= knifePurchasePrice) {
         knifeUpgradeButton.disabled = false;
     }
+    if(cheeseCount >= mousetronautPurchasePrice && mousetronautCount === 0) {
+        mousetronautUpgradeButton.disabled = false;
+    }
 }
 
 function isKnifeActivated() {
     if(knifeActivated) {
         cheeseCount = cheeseCount += knifeClickModifier;
+        totalCheeseCount.innerHTML = cheeseCount;
     }
+    return cheeseCount;
+}
+
+function isMousetronautActivated() {
+    if(mousetronautActivated) {
+        cheeseCount = cheeseCount += mousetronautClickModifier;
+        totalCheeseCount.innerHTML = cheeseCount;
+    }
+    return cheeseCount;
 }
