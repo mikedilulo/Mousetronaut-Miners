7097921d6accd4d4d9839b11329ae5a0e36a8252
michaeladilulo <michaeladilulo@gmail.com>
2023-07-16 01:23:18 -0600

    Knife Upgrade Button - Purchase, Mod, Enable

--- app.js
+++ app.js
@@ -1,15 +1,67 @@
 let cheeseCount = 0;
 
+const gameUpgrades = {
+    knives: {
+        purchasePrice: 5,
+        clickModifier: 7,
+        activated: false
+    },
+    mousetronauts: {
+        purchasePrice: 7,
+        clickModifer: 10,
+        activated: false
+    }
+}
+
+let knifePurchasePrice = gameUpgrades.knives.purchasePrice;
+let knifeActivated = gameUpgrades.knives.activated;
+let knifeClickModifier = gameUpgrades.knives.clickModifier;
+let mousetronautPurchasePrice = gameUpgrades.mousetronauts.purchasePrice;
+
 const totalCheeseCount = document.querySelector('.cheese-count');
 const clickToGenerateCheeseResource = document.getElementById('cheeseResource');
+const allResourceUpgradeButtons = document.querySelectorAll('button');
+const knifeUpgradeButton = document.querySelector('.cheese-knife-upgrade-btn');
+const mousetronautUpgradeButton = document.querySelector('.cheese-mousetronaut-upgrade-btn')
+
+disableButtonsAtStart();
 
 clickToGenerateCheeseResource.addEventListener('click', () => {
 mineCheese();
-}); 
+});
+
+knifeUpgradeButton.addEventListener('click', () => {
+    purchaseCheeseKnifeUpgrade();
+})
 
 function mineCheese() {
-    cheeseCount = cheeseCount + 1;
+    cheeseCount++;
     totalCheeseCount.innerHTML = cheeseCount;
-    console.log(cheeseCount)
-    return cheeseCount;
+    enableButtonsForUpgrades();
+    isKnifeActivated();
+}
+
+function disableButtonsAtStart() {
+    for(upgrade of allResourceUpgradeButtons) {
+        upgrade.disabled = true;
+    }
+}
+
+function purchaseCheeseKnifeUpgrade() {
+    knifeActivated = true;
+    cheeseCount -= knifePurchasePrice;
+    mineCheese();
+    return knifeActivated;
+}
+
+function enableButtonsForUpgrades() {
+    if(cheeseCount >= knifePurchasePrice) {
+        knifeUpgradeButton.disabled = false;
+    }
+}
+
+function isKnifeActivated() {
+    if(knifeActivated) {
+        cheeseCount = cheeseCount += knifeClickModifier;
+    }
 }
